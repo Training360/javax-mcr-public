61da3a11e78e90ac45e0fac92c5005e5e597fb2b
Istvan Viczian <viczian.istvan@gmail.com>
2026-04-23 17:41:13 +0200

    Nem használt import

--- demos-on-videos-spring-boot-4/employees-m04-l02-spring-data-jpa/src/test/java/employees/HelloControllerTest.java
+++ demos-on-videos-spring-boot-4/employees-m04-l02-spring-data-jpa/src/test/java/employees/HelloControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
